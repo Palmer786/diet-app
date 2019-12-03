f4dcf2b4eb8e9d8f131c340314113cc1512322a5
Kamil Krol <aeilon2332@gmail.com>
2019-12-03 15:01:16 +0100

    Added Nav.js Diet.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BreakfastProvider } from "./components/DietContext";
 import UserInfo from "./components/UserInfo";
+import Nav from "./components/Nav";
+import Diet from "./components/Diet";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <BreakfastProvider>
         <UserInfo />
+        <Diet />
       </BreakfastProvider>
     </div>
   );
---
+++ src/components/Diet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Diet = () => {
+  return (
+    <div className="diet">
+      <div className="diet-breakfast"></div>
+      <div className="diet-dinner"></div>
+      <div className="diet-supper"></div>
+    </div>
+  );
+};
+export default Diet;
