957d01de2fe6deb9e12f6428cbea44df77097ac3
palmer786 <palmer786786@gmail.com>
2019-12-04 13:35:23 +0100

    changed DietContext

--- src/components/DietContext.js
+++ src/components/DietContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const DietContext = createContext();
 
 export const DietProvider = props => {
-  const [items, getItems] = useState([
+  const [items, setItems] = useState([
     {
       key: "breakfast",
       food: [
