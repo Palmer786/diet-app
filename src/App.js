7ef69a4bc19f4ec066badc6e781d665355039b7d
palmer786 <palmer786786@gmail.com>
2019-12-04 14:31:32 +0100

    added router, routes and links in nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { DietProvider } from "./components/DietContext";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Breakfast from "./components/Breakfast";
+import Dinner from "./components/Dinner";
+import Supper from "./components/Supper";
 import UserInfo from "./components/UserInfo";
 import Nav from "./components/Nav";
 import Diet from "./components/Diet";
+import About from "./components/About";
 import "./App.css";
 import PersonalInfo from "./components/PersonalInfo";
 import PersonalCal from "./components/PersonalCal";
@@ -10,11 +15,25 @@ import PersonalCal from "./components/PersonalCal";
 const App = () => {
   return (
     <div>
-      <DietProvider>
-        <Nav />
-        <PersonalInfo />
-        <Diet />
-      </DietProvider>
+      <Router>
+        <DietProvider>
+          <Nav />
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <React.Fragment>
+                <PersonalInfo />
+                <Diet />
+              </React.Fragment>
+            )}
+          />
+          <Route path="/about" component={About} />
+          <Route path="/breakfast" component={Breakfast} />
+          <Route path="/dinner" component={Dinner} />
+          <Route path="/supper" component={Supper} />
+        </DietProvider>
+      </Router>
     </div>
   );
 };
