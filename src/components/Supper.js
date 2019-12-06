36faa37e0b5973b0991c949df133667add577d00
Kamil Krol <aeilon2332@gmail.com>
2019-12-06 15:38:54 +0100

    css changes in breakfast dinner supper

--- src/components/Supper.js
+++ src/components/Supper.js
@@ -4,14 +4,25 @@ import { DietContext } from "./DietContext";
 const Supper = () => {
   const [items, setItems] = useContext(DietContext);
   return (
-    <div>
-      <h1>Supper</h1>
-      {items[2].food.map(item => (
-        <div key={item.id}>
-          <h3>{item.name}</h3>
-          <p>{item.calories}</p>
-        </div>
-      ))}
+    <div className="main">
+        <h1>SUPPER</h1>
+      <div className="search">
+
+        <input type="text" className="search-bar" />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="myList">
+        {items[2].food.map(item => (
+          <div className="product" key={item.id}>
+            <div className="product-name">
+              <h3>{item.name}</h3>
+            </div>
+            <div className="product-details">
+              <p>Calories: {item.calories}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
