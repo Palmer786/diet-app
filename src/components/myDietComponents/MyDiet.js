0671b18cfcfa18f43dcea3f4bb9e9a617873fea5
palmer786 <palmer786786@gmail.com>
2019-12-10 16:38:30 +0100

    a new meal  adding function has been added

--- src/components/myDietComponents/MyDiet.js
+++ src/components/myDietComponents/MyDiet.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Meal from "./Meal";
+import AddMealKeys from "./AddMealKey";
 
 const MyDiet = () => {
-  const mealKeys = ["breakfast", "dinner", "supper"]
+  const [mealKeys, setMealKeys] = useState(
+    JSON.parse(localStorage.getItem("mealKeys")) || [
+      "breakfast",
+      "dinner",
+      "supper"
+    ]
+  );
+  useEffect(() => {
+    localStorage.setItem("mealKeys", JSON.stringify(mealKeys));
+  }, [mealKeys]);
   return (
-    <div className="diet">
+    <div>
+      <div className="diet">
         {mealKeys.map(key => (
-            <div>
-                <Meal title={key} />
-            </div>
+          <div>
+            <Meal title={key} />
+          </div>
         ))}
+      </div>
+      <AddMealKeys setMealKeys={setMealKeys} />
     </div>
   );
 };
