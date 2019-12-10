cdf96ae2cc52b7efb0a1744a13e076f1e7f24782
palmer786 <palmer786786@gmail.com>
2019-12-10 15:13:15 +0100

    fixed error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import About from "./components/About";
-import MyDiet from "./components/myDietComponents/MyDiet";
 import MealDetails from "./components/myDietComponents/MealDetails";
+import Homepage from "./components/Homepage";
 import "./App.css";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <Router>
         <Nav />
-        <Route path="/" exact component={MyDiet} />
+        <Route path="/" exact component={Homepage} />
         <Route path="/about" exact component={About} />
         <Route path="/meals/:title" exact component={MealDetails} />
       </Router>
