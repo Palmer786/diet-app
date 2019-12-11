e21358124114609c74bbfce836af6e108b93a66a
palmer786 <palmer786786@gmail.com>
2019-12-11 13:52:20 +0100

    fixed bug

--- src/components/myDietComponents/DelMealKey.js
+++ src/components/myDietComponents/DelMealKey.js
@@ -3,7 +3,7 @@ import React from "react";
 const DelMealKey = ({ title, setMealKeys }) => {
   const delMealKey = () => {
     const array = JSON.parse(localStorage.getItem("mealKeys"));
-    setMealKeys(array.filter(key => key != title));
+    setMealKeys(array.filter(key => key !== title));
     localStorage.removeItem(`${title}`);
   };
   return <button onClick={delMealKey}>Delete</button>;
